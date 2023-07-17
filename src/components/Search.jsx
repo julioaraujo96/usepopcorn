f89b93c6f518a68b72a6cf00d4f9f73893524416
julioaraujo96 <julioaraujo.bs@gmail.com>
2023-07-17 11:44:37 +0100

    feat: focus search on mount

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,10 +1,27 @@
+import PropTypes from 'prop-types';
+import { useEffect, useRef } from 'react';
+
 export function Search({ query, onQuery }) {
+
+  const searchRef = useRef(null);
+
+  useEffect(() => {
+      searchRef.current.focus();
+  }, [])
+
   return (
     <input
       className='search'
       type='text'
       placeholder='Search movies...'
       value={query}
-      onChange={(e) => onQuery(e.target.value)} />
+      onChange={(e) => onQuery(e.target.value)} 
+      ref={searchRef}
+      />
   );
+}
+
+Search.propTypes = {
+  query: PropTypes.string,
+  onQuery: PropTypes.func,
 }
